45190c3792511d72ffe7900baaa3524102f33a28
Alejandro Antezana <alejandroantezanadelarivera@gmail.com>
2024-07-20 01:17:43 -0300

    Terminados metodos principales de arbol binario de busqueda V2

--- src/conjuntistas/dinamicas/ArbolBB_V2.java
+++ src/conjuntistas/dinamicas/ArbolBB_V2.java
@@ -3,118 +3,213 @@ package conjuntistas.dinamicas;
 public class ArbolBB_V2 {
     private NodoABB raiz;
 
+    //Constructor
     public ArbolBB_V2() {
         this.raiz = null;
     }
 
-    //Algoritmo Pertenece implementado de forma iterativa
-    public boolean pertenece(Comparable elem) {
-        /*Este algoritmo puede plantearse de forma iterativa dado que solo se recorre
-         * una rama del Arbol BB.*/
-        boolean encontrado = false;
-        NodoABB aux = this.raiz;
-
-        while (!encontrado && aux != null) {
-            if (elem.compareTo(aux.getElemento()) == 0) {
-                encontrado = true;
-            } else {
-                if (elem.compareTo(aux.getElemento()) < 0) {
-                    aux = aux.getIzquierdo();
-                } else {
-                    aux = aux.getDerecho();
-                }
-            }
-        }
-        return encontrado;
-    }
-
-    //Algoritmo pertenece implementado de forma recursiva
-    /*public boolean perteneceRecursivo(Comparable elem) {
-        return perteneceRecursivoAux(this.raiz, elem);
-    }
-    private boolean perteneceRecursivoAux(NodoABB nodo, Comparable elem) {
-        boolean encontrado = false;
-        if (nodo != null) {
-            if (elem.compareTo(nodo.getElemento()) == 0) {
-                encontrado = true;
-            } else {
-                if (elem.compareTo(nodo.getElemento()) < 0) {
-                    encontrado = perteneceRecursivoAux(nodo.getIzquierdo(), elem);
-                } else {
-                    encontrado = perteneceRecursivoAux(nodo.getDerecho(), elem);
-                }
-            }
-        }
-        return encontrado;
-    } */
-    public boolean insertar(Comparable elemento) {
-        /*Recibe un elemento y lo agrega en el árbol de manera ordenada. Si el elemento ya se encuentra
-        en el árbol no se realiza la inserción. Devuelve verdadero si el elemento se agrega a la estructura y
-        falso en caso contrario.*/
-        boolean exito = true;
-        if (this.raiz == null) {
-            this.raiz = new NodoABB(elemento, null, null);
-        } else {
-            exito = insertarAux(this.raiz, elemento);
-        }
-        return exito;
-    }
-
-    //Algoritmo insertar implementado de manera iterativa
-    public boolean insertarAux(NodoABB nodo, Comparable elemento) {
+    public boolean insertar(Comparable valor){
         boolean succes = true;
-        boolean insertado = false;
-        while(!insertado) {
-            if (elemento.compareTo(nodo.getElemento()) == 0) {
-                //error de elemento repetido
-                succes = false;
-                insertado = true;
-            } else if (elemento.compareTo(nodo.getElemento()) < 0) { //me voy a la izquierda
-                if (nodo.getIzquierdo() == null) {
-                    NodoABB nuevoHI = new NodoABB(elemento, null, null);
-                    nodo.setIzquierdo(nuevoHI);
-                    insertado = true;
-                } else {
-                    nodo = nodo.getIzquierdo();
-                }
-            } else {
-                if (nodo.getDerecho() == null) {
-                    NodoABB nuevoHD = new NodoABB(elemento, null, null);
-                    nodo.setDerecho(nuevoHD);
-                    insertado = true;
-                } else {
-                    nodo = nodo.getDerecho();
-                }
+
+        if(this.raiz != null){
+            //Si el arbol no está vacio llamo a un metodo auxiliar para realizar la insercion.
+            succes = insertarAux(this.raiz, valor);
+        }else {
+            //si el arbol esta vacio le asigno el valor a la raiz.
+            NodoABB raiz = new NodoABB(valor, null, null);
+            this.raiz = raiz;
+            succes = true;
+        }
+        return succes;
+    }
+
+    private boolean insertarAux(NodoABB nodo, Comparable valor){
+        boolean succes = true;
+
+        //Si el elemento guardado en el nodo es igual al valor que quiero insertar
+        if(valor.compareTo(nodo.getElemento()) == 0){
+            succes = false;
+        } else if(valor.compareTo(nodo.getElemento()) < 1) {
+            //si el valor es menor al elemento del nodo pregunto si tiene Hijo Izq.
+            if(nodo.getIzquierdo() != null){
+                //Si tiene HI vuelvo a llamar recursivamente con el hijo izq.
+                succes = insertarAux(nodo.getIzquierdo(), valor);
+            }else{
+                //Si no tiene HI creo un nuevo nodo y le asigno el valor.
+                NodoABB nuevoHijoIzq = new NodoABB(valor, null, null);
+                //Enlazo al nuevo nodo como hijo izquierdo.
+                nodo.setIzquierdo(nuevoHijoIzq);
+            }
+        }else{
+            //Si el valor es mayor al elemento del nodo pregunto si tiene Hijo derecho.
+            if(nodo.getDerecho() != null){
+                //Si tiene vuelvo a llamar recursivamente con el Hijo derecho.
+                succes = insertarAux(nodo.getDerecho(), valor);
+            }else{
+                //Si no tiene HD creo el nuevo nodo y le asigno valor.
+                NodoABB nuevoHijoDer = new NodoABB(valor, null, null);
+                //enlazo el nuevo nodo como hijo derecho.
+                nodo.setDerecho(nuevoHijoDer);
             }
         }
         return succes;
     }
-    //Algoritmo insertar implementado de manera recursiva
-    /*private boolean insertarAux(NodoABB nodo, Comparable elemento) {
-        // precondicion: n no es nulo
-        boolean exito = true;
 
-        if ((elemento.compareTo(nodo.getElemento()) == 0)) {
-            //Reporta error: Elemento repetido
-            exito = false;
-        } else if ((elemento.compareTo(nodo.getElemento()) < 0)) {
-            // elemento es menor que n.getElem()
-            // si tiene HI baja a la izquierda, sino agrega elemento
-            if (nodo.getIzquierdo() != null) {
-                exito = insertarAux(nodo.getIzquierdo(), elemento);
-            } else {
-                nodo.setIzquierdo(new NodoABB(elemento, null, null));
-            }
-        } else { // elemento es mayor que n.getElem()
-            // si tiene HD baja a la derecha, sino agrega elemento
-            if (nodo.getDerecho() != null) {
-                exito = insertarAux(nodo.getDerecho(), elemento);
-            } else {
-                nodo.setDerecho(new NodoABB(elemento, null, null));
+    public boolean pertenece(Comparable valor){
+        //Devuelve True si el valor pasado por parametro se encuentra en el arbol. Devuelve False si no se encuentra.
+        boolean pertenece = false;
+        if(this.raiz != null){
+            pertenece = perteneceAux(this.raiz, valor);
+        }
+        return pertenece;
+    }
+
+    private boolean perteneceAux(NodoABB nodo, Comparable valor){
+        boolean encontrado = false;
+
+        if(valor.compareTo(nodo.getElemento()) == 0){
+          //si valor es igual al elemento del nodo
+          encontrado = true;
+        } else {
+            if(valor.compareTo(nodo.getElemento()) < 0){
+                //SI valor < elemento entonces busco en el subarbol izquierdo.
+                if(nodo.getIzquierdo() != null) {
+                    encontrado = perteneceAux(nodo.getIzquierdo(), valor);
+                }
+            }else {
+                //Si valor > elemento entonces busco en el subarbol Derecho
+                if(nodo.getDerecho() != null){
+                encontrado = perteneceAux(nodo.getDerecho(), valor);
+                }
             }
         }
-        return exito;
-    } */
+        return encontrado;
+    }
+
+    public boolean eliminar(Comparable valor) {
+        /* Recibe el elemento que se desea eliminar y se procede a removerlo del árbol. Si no se encuentra
+        el elemento no se puede realizar la eliminación. Devuelve verdadero si el elemento se elimina de la
+        estructura y falso en caso contrario.*/
+        boolean succes = false;
+
+        if(this.raiz != null){
+            succes = eliminarAux(this.raiz, null, valor);
+        }
+        return succes;
+    }
+
+    private boolean eliminarAux(NodoABB nodo, NodoABB padre, Comparable valor){
+        boolean eliminado = false;
+
+        if(valor.compareTo(nodo.getElemento()) == 0){
+            //Si el nodo contiene el elemento analizo que caso aplicar
+            if(nodo.getIzquierdo() == null && nodo.getDerecho() == null){
+                //Caso 1: El nodo es una hoja
+                caso1(valor, padre);
+            } else if(nodo.getIzquierdo() != null && nodo.getDerecho() != null) {
+                //Caso 3: El nodo tiene ambos hijos
+                caso3(nodo);
+            }else {
+                //Caso 2: El nodo tiene un solo hijo
+                caso2(valor, nodo, padre);
+            }
+            eliminado = true;
+        }else{
+            //Si el nodo no contiene el valor buscado sigo buscando.
+            int value = valor.compareTo(nodo.getElemento());
+            if (value < 1){ //Voy al subarbol izquierdo
+                if(nodo.getIzquierdo() != null) {
+                    eliminado = eliminarAux(nodo.getIzquierdo(), nodo, valor);
+                }
+            }else{ //voy al subarbol derecho
+                if(nodo.getDerecho() != null) {
+                    eliminado = eliminarAux(nodo.getDerecho(), nodo, valor);
+                }
+            }
+
+        }
+        return eliminado;
+    }
+
+    private void caso1(Comparable valor, NodoABB padre){
+        if (padre == null){
+            //caso especial de que el elemento a eliminar sea la raiz del arbol
+            this.raiz = null;
+        }else {
+            int aux = valor.compareTo(padre.getElemento());
+            if(aux < 1){
+                //si valor < padre entonces elimino el Hijo Izquierdo
+                padre.setIzquierdo(null);
+            }else {
+                //caso contrario elimino el Hijo derecho
+                padre.setDerecho(null);
+            }
+        }
+    }
+
+    private void caso2(Comparable valor, NodoABB nodo, NodoABB padre){
+        NodoABB hijoIzquierdo = nodo.getIzquierdo();
+        NodoABB hijoDerecho = nodo.getDerecho();
+
+        if(padre == null){
+            //caso especial que se elimine la raiz con un solo hijo.
+            if (hijoIzquierdo != null){
+                this.raiz = hijoIzquierdo;
+            }else{
+                this.raiz = hijoDerecho;
+            }
+        }else{
+            int aux = valor.compareTo(padre.getElemento());
+            if(aux < 1){ //si el nodo a eliminar esta a la izquierda del padre
+                if(hijoIzquierdo != null){
+                    padre.setIzquierdo(hijoIzquierdo);
+                }else{
+                    padre.setIzquierdo(hijoDerecho);
+                }
+            }else{
+                if(hijoIzquierdo != null){
+                    padre.setDerecho(hijoIzquierdo);
+                }else{
+                    padre.setDerecho(hijoDerecho);
+                }
+            }
+        }
+    }
+
+    /*Usar el candidato B: el menor elemento del subArbol derecho de nodo. */
+    private void caso3(NodoABB nodo){
+        /*Las variables nodoActual y nodoSiguiente sirven para poder desvincular el nodo hoja, que corresponde
+        * al candidato B.*/
+        NodoABB nodoActual = nodo.getDerecho();
+        NodoABB nodoSiguiente = nodo.getDerecho().getIzquierdo();
+
+        if (nodoSiguiente == null){
+            nodo.setElemento(nodoActual.getElemento());
+            nodo.setDerecho(null);
+        }else {
+            //iniciamos la busqueda en la raiz del subarbol derecho de nodo
+            while (nodoSiguiente != null) {
+                if (nodoSiguiente.getIzquierdo() == null) {
+                    //pregunto si encontre la hoja
+                    nodo.setElemento(nodoSiguiente.getElemento()); //si la encuentro entonces le asigno su valor a nodo
+                    nodoActual.setIzquierdo(null); //luego desvinculo la hoja del arbol
+                }
+                //actualizamos los valores del bucle
+                nodoActual = nodoActual.getIzquierdo();
+                nodoSiguiente = nodoSiguiente.getIzquierdo();
+            }
+        }
+    }
+
+    public boolean esVacio(){
+        /*Devuelve falso si hay al menos un elemento en el árbol y verdadero en caso contrario.*/
+        return this.raiz == null;
+    }
+
+    public void vaciar(){
+        /*Vacia el arbol actual asignando null a la raiz.*/
+       this.raiz = null;
+    }
 
    public String toString() {
         return toStringAux(this.raiz);
--- src/test/conjuntistas/TestABB.java
+++ src/test/conjuntistas/TestABB.java
@@ -1,58 +1,58 @@
 package test.conjuntistas;
 
-import conjuntistas.dinamicas.ArbolBB;
-
+//import conjuntistas.dinamicas.ArbolBB;
+import conjuntistas.dinamicas.ArbolBB_V2;
 
 
 public class TestABB {
     public static void main(String[] args) {
-       //generar un test para probar todas las operaciones del arbol binario de busqueda
-         ArbolBB arbol = new ArbolBB();
-            System.out.println("Arbol vacio: " + arbol.toString());
-            System.out.println("Es vacio: " + arbol.esVacio());
-            System.out.println("Insertar 10: " + arbol.insertar(10));
-            System.out.println("Insertar 5: " + arbol.insertar(5));
-            System.out.println("Insertar 15: " + arbol.insertar(15));
-            System.out.println("Insertar 3: " + arbol.insertar(3));
-            System.out.println("Insertar 7: " + arbol.insertar(7));
-            System.out.println("Insertar 12: " + arbol.insertar(12));
-            System.out.println("Insertar 20: " + arbol.insertar(20));
-            System.out.println("Insertar 1: " + arbol.insertar(1));
-            System.out.println("Insertar 4: " + arbol.insertar(4));
-            System.out.println("Insertar 6: " + arbol.insertar(6));
-            System.out.println("Insertar 8: " + arbol.insertar(8));
-            System.out.println("Insertar 11: " + arbol.insertar(11));
-            System.out.println("Insertar 13: " + arbol.insertar(13));
-            System.out.println("Insertar 17: " + arbol.insertar(17));
-            System.out.println("Insertar 25: " + arbol.insertar(25));
-            System.out.println("Insertar 2: " + arbol.insertar(2));
-            System.out.println("Insertar 9: " + arbol.insertar(9));
-            System.out.println("Insertar 14: " + arbol.insertar(14));
-            System.out.println("Insertar 16: " + arbol.insertar(16));
-            System.out.println("Insertar 18: " + arbol.insertar(18));
-            System.out.println("Insertar 26: " + arbol.insertar(26));
-            System.out.println("Insertar 19: " + arbol.insertar(19));
-            System.out.println("Insertar 21: " + arbol.insertar(21));
-            System.out.println("Insertar 22: " + arbol.insertar(22));
-            System.out.println("Insertar 10: " + arbol.insertar(10));
+        //generar un test para probar todas las operaciones del arbol binario de busqueda
+        ArbolBB_V2 arbol = new ArbolBB_V2();
+        System.out.println("-----PRUEBA METODO toString(), esVacio() e insertar()-----");
+        System.out.println("Arbol vacio: " + arbol.toString());
+        System.out.println("Es vacio: " + arbol.esVacio());
+        System.out.println("Insertar 10: " + arbol.insertar(10));
+        System.out.println("Insertar 7: " + arbol.insertar(7));
+        System.out.println("Insertar 4: " + arbol.insertar(4));
+        System.out.println("Insertar 9: " + arbol.insertar(9));
+        System.out.println("Insertar 23: " + arbol.insertar(23));
+        System.out.println("Insertar 15: " + arbol.insertar(15));
+        System.out.println("Insertar 12: " + arbol.insertar(12));
+        System.out.println("Insertar 25: " + arbol.insertar(25));
+        System.out.println("Insertar 24: " + arbol.insertar(24));
+        System.out.println("Insertar 30: " + arbol.insertar(30));
+        System.out.println("Insertar 27: " + arbol.insertar(27));
+        System.out.println("Insertar elemento Repetido 27. Devuelve False: " + arbol.insertar(27));
+        System.out.println("Insertar elemento Repetido 22. Devuelve False: " + arbol.insertar(23));
+        System.out.println("");
+        System.out.println("-----COMPRUEBO EL ARBOL ACTUAL Y PROBAMOS METODO PERTENECE-----");
+        System.out.println("Arbol: " + arbol.toString());
+        System.out.println("Es vacio: " + arbol.esVacio());
+        System.out.println("Pertenece 10. Devuelve true: " + arbol.pertenece(10));
+        System.out.println("Pertenece 9. Devuelve true: " + arbol.pertenece(9));
+        System.out.println("Pertenece 30. Devuelve true: " + arbol.pertenece(30));
+        System.out.println("Pertenece 22. Devuelve false: " + arbol.pertenece(22));
+        System.out.println("Pertenece 1. Devuelve false: " + arbol.pertenece(1));
+        System.out.println("");
+        System.out.println("-----PROBAMOS EL METODO ELIMINAR-----");
+        System.out.println("+++++PRUEBA ELEMENTO QUE NO EXISTE EN EL ARBOL+++++");
+        System.out.println("Eliminar 1. Devuelve false: " + arbol.eliminar(1));
+        System.out.println("Eliminar 66. Devuelve false: " + arbol.eliminar(66));
+        System.out.println("");
+        System.out.println("+++++PRUEBA ELIMINAR CASO 1+++++");
+        System.out.println("Eliminar 9. Devuelve true: " + arbol.eliminar(9));
+        System.out.println("Eliminar 24. Devuelve true: " + arbol.eliminar(24));
+        System.out.println("");
+        System.out.println("+++++PRUEBA ELIMINAR CASO 2+++++");
+        System.out.println("Eliminar 7. Devuelve true: " + arbol.eliminar(7));
+        System.out.println("Eliminar 30. Devuelve true: " + arbol.eliminar(30));
+        System.out.println("");
+        System.out.println("+++++PRUEBA ELIMINAR CASO 3+++++");
+        System.out.println("Eliminar 23. Devuelve true: " + arbol.eliminar(23));
+        System.out.println("");
+        System.out.println("-----IMPRIMO ARBOL RESULTANTE-----");
+        System.out.println(arbol.toString());
 
-            System.out.println("Arbol: " + arbol.toString());
-            System.out.println("Es vacio: " + arbol.esVacio());
-            System.out.println("Pertenece 10: " + arbol.pertenece(10));
-            System.out.println("Pertenece 33: " + arbol.pertenece(5));
-            System.out.println("Pertenece 26: " + arbol.pertenece(15));
 
-            System.out.println("Eliminar 10: " + arbol.eliminar(10));
-            System.out.println("Eliminar 5: " + arbol.eliminar(5));
-            System.out.println("Eliminar 14: " + arbol.eliminar(15));
-            System.out.println("Eliminar 3: " + arbol.eliminar(3));
-
-            System.out.println("Arbol: " + arbol.toString());
-
-            System.out.println("Clonar: " + arbol.clone());
-            System.out.println("Vaciar: ");
-            arbol.vaciar();
-            System.out.println("Arbol vacio: " + arbol.toString());
-            System.out.println("Es vacio: " + arbol.esVacio());
     }
 }
