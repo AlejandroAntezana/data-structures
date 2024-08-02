71629c0de548d80a411c439a54aa758329afe8e0
Alejandro Antezana <alejandroantezanadelarivera@gmail.com>
2024-08-01 21:42:16 -0300

    implementacion incompleta de AVL

--- src/conjuntistas/dinamicas/NodoAVL.java
+++ src/conjuntistas/dinamicas/NodoAVL.java
@@ -1,32 +1,57 @@
 package conjuntistas.dinamicas;
 
-public class NodoAVL extends NodoABB{
+public class NodoAVL{
     //Agregamos el atributo de altura para el nodo
+    Comparable elemento;
+    private NodoAVL izquierdo;
+    private NodoAVL derecho;
     private int altura;
 
-    //Constructor redefinido para avl
-    public NodoAVL(Comparable elem, NodoAVL izq, NodoAVL der){
-        super(elem, izq, der);
-        this.altura = 0;
+    //constructor
+    public NodoAVL(Comparable elem){
+        this.elemento = elem;
+        this.izquierdo = null;
+        this.derecho = null;
+        this.altura = 0; //seria la altura del subarbol que tiene a este nodo como raiz
+    }
+
+    //observadores
+    public Comparable getElemento(){
+        return this.elemento;
+    }
+    public void setElemento(Comparable elem){
+        this.elemento = elem;
+    }
+    public NodoAVL getIzquierdo(){
+        return this.izquierdo;
+    }
+    public void setIzquierdo(NodoAVL izq){
+        this.izquierdo = izq;
+    }
+    public NodoAVL getDerecho(){
+        return this.derecho;
+    }
+    public void setDerecho(NodoAVL der){
+        this.derecho = der;
+    }
+    public int getAltura(){
+        return this.altura;
+    }
+    public void setAltura(int alt){
+        this.altura = alt;
     }
 
     //Las operaciones de acceso y modificacion de la altura
-    public static int obtenerAltura(NodoAVL nodo){
-        return nodo.altura;
-    }
-
-    public static void recalcularAltura(NodoAVL nodo){
+    public void recalcularAltura(){
         int altIzq = -1;
         int altDer = -1;
-        if(nodo.getIzquierdo() != null){
-            //En este caso realizamos un casteo de la clase padre a la clase hija.
-            altIzq = obtenerAltura((NodoAVL)nodo.getIzquierdo());
+        if(this.izquierdo != null){
+            altIzq = this.izquierdo.getAltura();
         }
-        if(nodo.getDerecho() != null){
-            altDer = obtenerAltura((NodoAVL)nodo.getDerecho());
+        if(this.derecho != null){
+            altDer = this.derecho.getAltura();
         }
-        nodo.altura = Math.max(altIzq, altDer) + 1;
+        this.altura = Math.max(altIzq, altDer) + 1;
     }
-
 
 }
